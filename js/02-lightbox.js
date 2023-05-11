4ae4f4d0a782b0ccb008d3965b29f4fa59f37167
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-05-11 20:48:12 +0300

    ./

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const imageList= document.querySelector('.gallery');
+const imagesListItems = createCards(galleryItems)
+imageList.insertAdjacentHTML('beforeend', imagesListItems)
+imageList.addEventListener('click',onContainerOpen)
+function createCards(items){
+return items.map(({original,preview,description})=>`
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+ .join('')`)
+}
+function onContainerOpen(event){
+    event.preventDefault()
+    if(!event.target.nodeName !=='IMG'){
+        return
+    }
+    console.log(event.target)
+    }
