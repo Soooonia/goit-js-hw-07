959ce64b72973091b87cf09932e706c70bec827d
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-05-11 14:47:45 +0300

    ./

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1)Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+ const imageList= document.querySelector('.gallery');
+ const imagesListItems = createCards(galleryItems)
+imageList.insertAdjacentHTML('beforeend', imagesListItems)
+imageList.addEventListener('click',onContainerOpen)
+function createCards(items){
+return items.map(({original,preview,description})=>`
+<li class="gallery__item">
+  <a class="gallery__link" 
+  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`)
+  .join('')  
+}
+
+function onContainerOpen(event){
+event.preventDefault()
+if(!event.target.classList.contains('gallery__image')){
+    return
+}
+console.log(event.target)
+}
